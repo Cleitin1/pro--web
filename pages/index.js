52d5e37eb4ee92642e0f1e15871285312291b05b
Hiago Martins <hiagomartinsitu@gmail.com>
2022-10-07 16:53:07 -0300

    Alterando codigo

--- pages/index.js
+++ pages/index.js
@@ -90,7 +90,7 @@ export default function Home() {
               <i className="fa fa-download" /> Baixar Resumo
             </button>
             {/* Grid for pricing tables */}
-            <h3 className="w3-padding-16 w3-text-light-grey">Meu preço</h3>
+            <h3 className="w3-padding-16 w3-text-dark-grey">Meu preço</h3>
             <div className="w3-row-padding" style={{margin: '0 -16px'}}>
               <div className="w3-half w3-margin-bottom">
                 <ul className="w3-ul w3-white w3-center w3-opacity w3-hover-opacity-off">
